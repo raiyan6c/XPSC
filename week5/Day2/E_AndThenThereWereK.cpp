9c8304b1733857ffb76d976f63abf0ee2befa179
Raiyanul Alam <163655040+raiyan6c@users.noreply.github.com>
2024-04-20 23:20:00 +0600

    added week5 day2

---
+++ week5/Day2/E_AndThenThereWereK.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define vi vector<long long>
+#define si set<long long>
+#define pi priority_queue<long long>
+#define stc stack<ll>
+using namespace std;
+void solve()
+{
+    ll n,x=1;
+    cin>>n;
+    ll d=0;
+    bool hu=false;
+
+    // for(ll i=31;i>0;i--)
+    // {
+    //     x=n&(1<<i);
+    //     // cout<<x<<"Hu\n";
+    //     if(x==0)
+    //     {
+    //         // cout<<i<<"ha"<<x<<"hu\n";
+    //         d=i;
+    //         break;
+    //     }
+    // }
+    // cout<<(1<<d)-1<<"\n";
+    while(x*2<=n)
+    {
+        x*=2;
+    }
+    cout<<x-1<<endl;
+
+
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
---
+++ week5/Day2/G_MinOrSum.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define vi vector<long long>
+#define si set<long long>
+#define pi priority_queue<long long>
+#define stc stack<ll>
+using namespace std;
+void solve()
+{
+    ll n,x=0;
+    cin>>n;
+    ll a[n];
+    for(ll i=0;i<n;i++)
+    {
+        cin>>a[i];
+        x|=a[i];
+    }
+    cout<<x<<"\n";
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
