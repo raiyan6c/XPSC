67b2ddfb4e6c50284096ea26f7981f2a2eac9ef6
Raiyanul Alam <163655040+raiyan6c@users.noreply.github.com>
2024-04-23 23:40:15 +0600

    added week5 day5

---
+++ week5/Day5/O_XORwice.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define vi vector<long long>
+#define si set<long long>
+#define pi priority_queue<long long>
+#define stc stack<ll>
+using namespace std;
+void solve()
+{
+    ll a,b;
+    cin>>a>>b;
+    ll x=a^b;
+    cout<<x<<"\n";
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
